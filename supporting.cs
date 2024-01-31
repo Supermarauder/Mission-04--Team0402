f28f380abcbfb6030c883d2b381ff59e431e075e
Supermarauder <119464479+Supermarauder@users.noreply.github.com>
2024-01-31 13:43:41 -0700

    created the supporting class and made frame of receiveBoard method

---
+++ supporting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_04__Team0402
+{
+    internal class supporting
+    {
+        public void receiveBoard()
+        {
+
+        }
+    }
+}
