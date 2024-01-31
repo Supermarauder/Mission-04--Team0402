12f406f1315107c7973d8feed0bb35b1f74addba
Supermarauder <119464479+Supermarauder@users.noreply.github.com>
2024-01-31 15:07:49 -0700

    working on potential ways to print the board, not complete yet

--- supporting.cs
+++ supporting.cs
@@ -15,9 +15,30 @@ namespace Mission_04__Team0402
             //loop to print out
             for (int iCount = 0;  iCount < asBoard.Length; iCount++) 
             {
-                Console.Write(asBoard[iCount]);
+                if (iCount % 3 == 0)
+                {
+                    Console.WriteLine(asBoard[iCount]);
+                }
+                else
+                {
+                    Console.Write(asBoard[iCount]);
+                }
                 //have a  built in counter that goes to the next line every 3 values
+
+                //maybe use an $ string method instead to print the board
+
+                Console.WriteLine($" | {asBoard[0]} | {asBoard[1]} | {asBoard[2]} ");
+                Console.WriteLine($" | {asBoard[3]} | {asBoard[4]} | {asBoard[5]} ");
+                Console.WriteLine($" | {asBoard[6]} | {asBoard[7]} | {asBoard[8]} ");
             }
+            return asBoard;
+        }
+        public string[] DetermineWinner(string[] asBoard) 
+        {0
+
+
+
+
             return asBoard;
         }
     }
