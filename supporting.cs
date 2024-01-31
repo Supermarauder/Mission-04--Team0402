c51614f2c49c7f5eb0af48833cca1319cf06baae
Supermarauder <119464479+Supermarauder@users.noreply.github.com>
2024-01-31 14:02:44 -0700

    putting together the receiving method

--- supporting.cs
+++ supporting.cs
@@ -8,9 +8,17 @@ namespace Mission_04__Team0402
 {
     internal class supporting
     {
-        public void receiveBoard()
+        public string[] receiveBoard(string[] asBoard)
         {
+            // array from 0-8 that is received 
 
+            //loop to print out
+            for (int iCount = 0;  iCount < asBoard.Length; iCount++) 
+            {
+                Console.Write(asBoard[iCount]);
+                //have a  built in counter that goes to the next line every 3 values
+            }
+            return asBoard;
         }
     }
 }
