ea52985c1edf3f9ec12e3c5461012abdd4810a8b
hecowart <haydenecowart@gmail.com>
2024-02-01 16:07:27 -0700

    tinker

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using supporting;
+﻿using Mission_04__Team0402;
 
 // create variables for do while and a counter to see who's turn it is.
 // If i = 1, then player 1's turn. if 2, player 2
@@ -7,6 +7,8 @@ int i = 1;
 //initiate array to pass to supporting class
 string[] board = new string[9] {"", "", "", "", "", "", "", "", ""};
 
+//initiate the class
+Supporting s = new Supporting; 
 
 // initiate variables for player1 and player2 moves
 string player1Move = ""; 
@@ -26,28 +28,6 @@ do
     // player 1's turn
     do
     {
-        /*check to see if there is a winner USE METHOD FOR THIS
-        * if determineWinner == player1;
-        *      winner = player1
-        *      Console.WriteLine(winner + " got 3 in a row! They Win!")
-        *      gameOver = true
-        * else if determineWinner == player2;
-        *      winner = player2
-        *      Console.WriteLine(winner + " got 3 in a row! They Win!")
-        *      gameOver = true
-        * else if determineWinner == tie;
-        *      winner = "no one"
-        *      Console.WriteLine("Tie Game! " + winner + " wins!")
-        *      gameOver = true
-        * else if determineWinner == undetermined;
-        *      continue with player 1's turn
-        * .
-        * 
-        *      
-        *
-        *
-        */
-
         // give basic instructions everytime their turn starts
         Console.WriteLine("Here is the board:");
         Console.WriteLine("USE METHOD TO PRINT THE BOARD HERE");
@@ -56,7 +36,33 @@ do
         Console.WriteLine("Type A to go in the MIDDLE LEFT position. Type S to go in the MIDDLE MIDDLE position. Type D to go in the MIDDLE RIGHT position");
         Console.WriteLine("Type Z to go in the BOTTOM LEFT position. Type X to go in the BOTTOM MIDDLE position. Type C to go in the BOTTOM RIGHT position");
         Console.WriteLine("Any other entry will cause the board and these instructions to be printed again.");
+        
+        //run function of checking a winner
+        s.determineWinner();
+
+        //receive the input for their turn
         player1Move = Console.ReadLine();
+
+        
+        if determineWinner == player1;
+             winner = player1
+             Console.WriteLine(winner + " got 3 in a row! They Win!")
+             gameOver = true
+        else if determineWinner == player2;
+             winner = player2
+             Console.WriteLine(winner + " got 3 in a row! They Win!")
+             gameOver = true
+        else if determineWinner == tie;
+             winner = "no one"
+             Console.WriteLine("Tie Game! " + winner + " wins!")
+             gameOver = true
+        else if determineWinner == undetermined;
+             continue with player 1's turn
+        
+        
+             
+
+        
 
         //use logic to ensure player1's move is valid. First check to see if it is one of the appropriate 9 letters,
         //then check to see if that part of the board has already been played on
