de3ffe24bf11e82281cf3f2f2e533b983f2c9401
Supermarauder <119464479+Supermarauder@users.noreply.github.com>
2024-02-01 11:32:29 -0700

    finished the  determineWinner and receiveBoard methods

--- supporting.cs
+++ supporting.cs
@@ -6,40 +6,92 @@ using System.Threading.Tasks;
 
 namespace Mission_04__Team0402
 {
-    internal class supporting
+    internal class Supporting
     {
-        public string[] receiveBoard(string[] asBoard)
+        public string receiveBoard(string[] asBoard)
         {
-            // array from 0-8 that is received 
+           //The Board array is received and put into the sResults string so it can be easily returned and printed
+           //The following print simulates a 3x3 grid
+         string sResults = $" | {asBoard[0]} | {asBoard[1]} | {asBoard[2]} ";
+                sResults += $"\n ------";
+                sResults += $"\n | {asBoard[3]} | {asBoard[4]} | {asBoard[5]} ";
+                sResults += $"\n ------";
+                sResults += $"\n | {asBoard[6]} | {asBoard[7]} | {asBoard[8]} ";
+                sResults += $"\n ------";
 
-            //loop to print out
-            for (int iCount = 0;  iCount < asBoard.Length; iCount++) 
+            return sResults;
+        }
+        public string determineWinner(string[] asBoard) 
+        {
+            // Check rows
+            for (int i = 0; i < 3; i++)
             {
-                if (iCount % 3 == 0)
+                if (asBoard[i * 3] == asBoard[i * 3 + 1] && asBoard[i * 3 + 1] == asBoard[i * 3 + 2])
                 {
-                    Console.WriteLine(asBoard[iCount]);
+                   if (asBoard[i * 3] == "X" )
+                    {
+                        return "Player 1 wins";
+                    }
+                   else
+                    {
+                        return "Player 2 wins";
+                    }
+                }
+            }
+
+            // Check columns
+            for (int i = 0; i < 3; i++)
+            {
+                if (asBoard[i] == asBoard[i + 3] && asBoard[i + 3] == asBoard[i + 6])
+                {
+                    if (asBoard[i] == "X")
+                    {
+                        return "Player 1 wins";
+                    }
+                    else
+                    {
+                        return "Player 2 wins";
+                    }
+                }
+            }
+
+            // Check diagonals
+            if (asBoard[0] == asBoard[4] && asBoard[4] == asBoard[8])
+            {
+                if (asBoard[0] == "X")
+                {
+                    return "Player 1 wins";
                 }
                 else
                 {
-                    Console.Write(asBoard[iCount]);
+                    return "Player 2 wins";
                 }
-                //have a  built in counter that goes to the next line every 3 values
-
-                //maybe use an $ string method instead to print the board
-
-                Console.WriteLine($" | {asBoard[0]} | {asBoard[1]} | {asBoard[2]} ");
-                Console.WriteLine($" | {asBoard[3]} | {asBoard[4]} | {asBoard[5]} ");
-                Console.WriteLine($" | {asBoard[6]} | {asBoard[7]} | {asBoard[8]} ");
             }
-            return asBoard;
-        }
-        public string[] DetermineWinner(string[] asBoard) 
-        {0
 
+            if (asBoard[2] == asBoard[4] && asBoard[4] == asBoard[6])
+            {
+                if (asBoard[2] == "X")
+                {
+                    return "Player 1 wins";
+                }
+                else
+                {
+                    return "Player 2 wins";
+                }
+            }
 
+            // Check for any open spaces
+            for (int i = 0; i < asBoard.Length; i++)
+            {
+                if (asBoard[i] == " ")
+                {
+                    return "The game continues!";
+                }
+            }
 
+            // No winner and no open spaces
+            return "Tie game";
 
-            return asBoard;
         }
     }
 }
