66a0060a280b924fc8aa9825f5accb6e933adb84
hecowart <haydenecowart@gmail.com>
2024-02-01 16:42:25 -0700

    add in P2 logic for input

--- Program.cs
+++ Program.cs
@@ -46,14 +46,16 @@ do
         if (player1Move == 'q')
         {
             position = 0;
-                if board[position] = ""
-                {
-                    board[position] = "X";
-                }
-                else
-                {
-                    Console.WriteLine("This position is filled. Try again.");
-                }
+            if board[position] = ""
+            {
+                board[position] = "X";
+                //up the i to send to player 2
+                i++;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
         }
         else if (player1Move == 'w')
         {
@@ -61,6 +63,7 @@ do
             if board[position] = ""
                 {
                 board[position] = "X";
+                i++;
             }
             else
             {
@@ -73,6 +76,7 @@ do
             if board[position] = ""
                 {
                 board[position] = "X";
+                i++;
             }
             else
             {
@@ -85,6 +89,7 @@ do
             if board[position] = ""
                 {
                 board[position] = "X";
+                i++;
             }
             else
             {
@@ -97,6 +102,7 @@ do
             if board[position] = ""
                 {
                 board[position] = "X";
+                i++;
             }
             else
             {
@@ -109,6 +115,7 @@ do
             if board[position] = ""
                 {
                 board[position] = "X";
+                i++;
             }
             else
             {
@@ -121,6 +128,7 @@ do
             if board[position] = ""
                 {
                 board[position] = "X";
+                i++;
             }
             else
             {
@@ -133,6 +141,7 @@ do
             if board[position] = ""
                 {
                 board[position] = "X";
+                i++;
             }
             else
             {
@@ -145,6 +154,7 @@ do
             if board[position] = ""
                 {
                 board[position] = "X";
+                i++;
             }
             else
             {
@@ -162,33 +172,156 @@ do
             Console.WriteLine("Any other entry will cause the board and these instructions to be printed again.");
             player1Move = Console.ReadLine();
         }
-
-        //add onto the i to switch to player 2 turn
-        i++;
-
-        /*
-        if determineWinner == player1;
-             winner = player1
-             Console.WriteLine(winner + " got 3 in a row! They Win!")
-             gameOver = true
-        else if determineWinner == player2;
-             winner = player2
-             Console.WriteLine(winner + " got 3 in a row! They Win!")
-             gameOver = true
-        else if determineWinner == tie;
-             winner = "no one"
-             Console.WriteLine("Tie Game! " + winner + " wins!")
-             gameOver = true
-        else if determineWinner == undetermined;
-             continue with player 1's turn
-        */
-
     } while (i == 1);
 
     // player 2's turn
     do
     {
+        // give basic instructions everytime their turn starts
+        Console.WriteLine("Here is the board:");
+        Console.WriteLine("USE METHOD TO PRINT THE BOARD HERE");
+        Console.WriteLine("Instructions:");
+        Console.WriteLine("Type Q to go in the UPPER LEFT position. Type W to go in the UPPER MIDDLE position. Type E to go in the UPPER RIGHT position");
+        Console.WriteLine("Type A to go in the MIDDLE LEFT position. Type S to go in the MIDDLE MIDDLE position. Type D to go in the MIDDLE RIGHT position");
+        Console.WriteLine("Type Z to go in the BOTTOM LEFT position. Type X to go in the BOTTOM MIDDLE position. Type C to go in the BOTTOM RIGHT position");
+        Console.WriteLine("Any other entry will cause the board and these instructions to be printed again.");
 
+        //run function of checking a winner
+        s.determineWinner();
+
+        //receive the input for their turn
+        player2Move = Console.ReadLine();
+
+        //decipher the user input
+        if (player2Move == 'q')
+        {
+            position = 0;
+            if board[position] = ""
+            {
+                board[position] = "X";
+                //down the i to send to player 1
+                i--;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
+        }
+        else if (player2Move == 'w')
+        {
+            position = 1;
+            if board[position] = ""
+                {
+                board[position] = "X";
+                i--;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
+        }
+        else if (player2Move == 'e')
+        {
+            position = 2;
+            if board[position] = ""
+                {
+                board[position] = "X";
+                i--;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
+        }
+        else if (player2Move == 'a')
+        {
+            position = 3;
+            if board[position] = ""
+                {
+                board[position] = "X";
+                i--;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
+        }
+        else if (player2Move == 's')
+        {
+            position = 4;
+            if board[position] = ""
+                {
+                board[position] = "X";
+                i--;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
+        }
+        else if (player2Move == 'd')
+        {
+            position = 5;
+            if board[position] = ""
+                {
+                board[position] = "X";
+                i--;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
+        }
+        else if (player2Move == 'z')
+        {
+            position = 6;
+            if board[position] = ""
+                {
+                board[position] = "X";
+                i--;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
+        }
+        else if (player2Move == 'x')
+        {
+            position = 7;
+            if board[position] = ""
+                {
+                board[position] = "X";
+                i--;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
+        }
+        else if (player2Move == 'c')
+        {
+            position = 8;
+            if board[position] = ""
+                {
+                board[position] = "X";
+                i--;
+            }
+            else
+            {
+                Console.WriteLine("This position is filled. Try again.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("You made an invalid entry.");
+            Console.WriteLine("USE METHOD TO PRINT THE BOARD HERE");
+            Console.WriteLine("Instructions:");
+            Console.WriteLine("Type Q to go in the UPPER LEFT position. Type W to go in the UPPER MIDDLE position. Type E to go in the UPPER RIGHT position");
+            Console.WriteLine("Type A to go in the MIDDLE LEFT position. Type S to go in the MIDDLE MIDDLE position. Type D to go in the MIDDLE RIGHT position");
+            Console.WriteLine("Type Z to go in the BOTTOM LEFT position. Type X to go in the BOTTOM MIDDLE position. Type C to go in the BOTTOM RIGHT position");
+            Console.WriteLine("Any other entry will cause the board and these instructions to be printed again.");
+            player2Move = Console.ReadLine();
+        }
     } while (i == 2);
 
 } while (gameOver == false);
